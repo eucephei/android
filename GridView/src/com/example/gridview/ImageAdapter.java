e62bc4643aa4930c717457491a10bc93e9b6084d
eucephei <eucephei@gmail.com>
2013-03-03 21:16:42 +0800

    GridView+OnItemClickListener

---
+++ GridView/src/com/example/gridview/ImageAdapter.java
@@ -0,0 +1,51 @@
+package com.example.gridview;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+ 
+public class ImageAdapter extends BaseAdapter {
+    private Context mContext;
+ 
+    // Keep all Images in array
+    public Integer[] mThumbIds = {
+            R.drawable.everest1, R.drawable.everest2, 
+            R.drawable.everest3, R.drawable.everest4, R.drawable.everest5,             
+            R.drawable.everest1_160x160, R.drawable.everest2_160x160,
+            R.drawable.everest3_160x160, R.drawable.everest4_160x160  
+    };
+
+    // Constructor
+    public ImageAdapter(Context c){
+        mContext = c;
+    }
+ 
+    @Override
+    public int getCount() {
+        return mThumbIds.length;
+    }
+ 
+    @Override
+    public Object getItem(int position) {
+        return mThumbIds[position];
+    }
+ 
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+ 
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ImageView imageView = new ImageView(mContext);
+        imageView.setImageResource(mThumbIds[position]);
+        imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+        imageView.setLayoutParams(new GridView.LayoutParams(150, 200));
+		imageView.setPadding(8, 10, 8, 10);
+        return imageView;
+    }
+ 
+}
