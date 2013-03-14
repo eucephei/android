d2d8998187bb5ac219fd59c6119eb72ff0d515c3
eucephei <eucephei@gmail.com>
2013-03-14 17:47:38 +0800

    android.app.AlertDialog

--- SelectionListView/src/com/example/selectionlistview/SelectionListViewActivity.java
+++ SelectionListView/src/com/example/selectionlistview/SelectionListViewActivity.java
@@ -1,20 +1,26 @@
 package com.example.selectionlistview;
 
-import android.os.Bundle;
+import android.app.AlertDialog;
 import android.app.ListActivity;
+import android.content.Context;
+import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 
-
 public class SelectionListViewActivity extends ListActivity {
+	
+	MenuDialog customMenuDialog;
 	TextView selection;
+	
     /** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle icicle) {
 		super.onCreate(icicle);
 		setContentView(R.layout.main);
+		
 		setListAdapter(new ArrayAdapter<String>(this,
 			android.R.layout.simple_list_item_single_choice,
 			WORLDCUP2010));
@@ -25,7 +31,8 @@ public class SelectionListViewActivity extends ListActivity {
 		listView.setItemsCanFocus(false);
 		listView.setChoiceMode(ListView.CHOICE_MODE_SINGLE);  
 		
-
+		// MenuDialog
+		registerForContextMenu(getListView());
 	}
 	
 	public void onListItemClick(ListView parent, View v, 
@@ -45,4 +52,34 @@ public class SelectionListViewActivity extends ListActivity {
 		"Slovakia", "Slovenia", "South Africa",  
 		"South Korea",  "Spain", "Switzerland",    
 		"United States", "Uruguay" };
+	
+	private class MenuDialog extends AlertDialog {
+		public MenuDialog(Context context) {
+			super(context);
+			setTitle("Custom Options Menu");
+			View cus_menu = getLayoutInflater().inflate(R.layout.custom_menu_layout, null);
+			setView(cus_menu);
+		}
+		
+		@Override
+		public boolean onKeyUp(int keyCode, KeyEvent event) {
+			if (keyCode == KeyEvent.KEYCODE_MENU) {
+				dismiss();
+				return true;
+			}
+			return super.onKeyUp(keyCode, event);
+		}
+	}
+	
+	@Override
+	public boolean onKeyUp(int keyCode, KeyEvent event) {
+		if (keyCode == KeyEvent.KEYCODE_MENU) {
+			if (customMenuDialog == null) {
+				customMenuDialog = new MenuDialog(this);
+			}
+			customMenuDialog.show();
+			return true;
+		}
+		return super.onKeyUp(keyCode, event);
+	}
 }
